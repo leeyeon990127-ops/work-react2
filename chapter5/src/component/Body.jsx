68d8bb2fe75bec6a4cc6ede80df7b4b703bfa8f5
yeonsoo <leeyeon990127@gmail.com>
2026-04-24 16:31:12 +0900

    jsx 스타일링

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import "./Body.css";
 
 function Body() {
-    const num = 200;
-
-    if (num % 2 === 0) {
-        return <div>{num}은(는) 짝수입니다.</div>;
-        } else {
-        return <div>{num}은(는) 홀수입니다.</div>;
-    }    
+    return (
+    <div className="body">    
+            <h1>body</h1>
+        </div>
+    );
 }
 export default Body;
