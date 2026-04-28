f9725a316a9820d9b88c91d1a71d6101cb161916
yeonsoo <leeyeon990127@gmail.com>
2026-04-28 18:55:52 +0900

    project1 : 5장 카운터 앱 만들기 6장 라이프 사이클

---
+++ project1/src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
+import Controller from "./component/Controller";
+import Viewer from "./component/Viewer";
+import Even from "./component/Even";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const didMountRef = useRef(false);
+
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      console.log("깜빡");
+    }, 1000);
+
+    return () => {
+      console.log("클린업");
+      clearInterval(intervalID);
+    };
+  });
+
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트!");
+    }
+  });
+
+  useEffect(() => {
+    console.log("컴포넌트 마운트");
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <input type={text} onChange={handleChangeText} />
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 && <Even />}
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
