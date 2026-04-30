d2f0efd084c37b8abb3171eb6e4081ae1b9f4065
yeonsoo <leeyeon990127@gmail.com>
2026-04-30 18:21:16 +0900

    ~p.370 최적화와 메모이제이션

--- project2/src/component/TodoList.jsx
+++ project2/src/component/TodoList.jsx
@@ -14,10 +14,27 @@ const TodoList = ({ todo, onUpdate, onDelete }) => {
           it.content.toLowerCase().includes(search.toLowerCase()),
         );
   };
+  const analyzeTodo = () => {
+    console.log("analyzeTodo 함수 호출");
+    const totalCount = todo.length;
+    const doneCount = todo.filter((it) => it.isdone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+  const { totalCount, doneCount, notDoneCount } = analyzeTodo();
 
   return (
     <div className="TodoList">
       <h4>Todo List 🐣</h4>
+      <div>
+        <div>총개수: {totalCount}</div>
+        <div>완료된 할 일: {doneCount}</div>
+        <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
